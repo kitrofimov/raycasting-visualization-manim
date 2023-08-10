3c5942db0a8c68bf4e44d093ebd8f57443a18e1c
Kirill <kitrofimov39@gmail.com>
2023-08-10 20:18:34 +0500

    Changes

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ class TrigonometryProofsScene(Scene):
                 "tip_width": 0.2,
                 "tip_height": 0.2,
                 "include_ticks": True,
-                "numbers_with_elongated_ticks": range(-5, 5),
+                "numbers_with_elongated_ticks": range(0, 5),
             },
             background_line_style={
                 "stroke_color": TEAL,
@@ -98,7 +98,7 @@ class TrigonometryProofsScene(Scene):
         # play number plane animations
         self.play(Succession(
             Create(number_plane, run_time=3),
-            Create(blocks),
+            DrawBorderThenFill(blocks),
             Write(labels),
             Write(x_ticks_labels),
             Write(y_ticks_labels)
@@ -114,27 +114,33 @@ class TrigonometryProofsScene(Scene):
             Write(ray_angle_label)
         ))
 
-        self.wait()
+        self.wait(2)
 
         # play brace animations
         self.play(Transform(player_label_1, player_label_2))
-        self.play(Succession(
+        self.play(AnimationGroup(
             Create(px_brace),
             Write(px_brace_label),
             Create(py_brace),
             Write(py_brace_label)
         ))
 
-        # play P(x+px, y+py) animation
-        player_label_3 = MathTex(r"P(x+px, y+py)")
-        player_label_3.move_to(RIGHT*3.5+UP*3)
-        self.play(Create(player_label_3, run_time=0.5))
+        # play P and theta explanation animation
+        group_explain = VGroup(
+            MathTex(r"P(x+px, y+py)").move_to(RIGHT*3.5+UP*0.5),
+            MathTex(r"\theta \textrm{ is ray angle}").move_to(RIGHT*3.5+DOWN*0.5),
+        )
+        self.play(Write(group_explain))
 
         self.wait(3)
 
         # uncreate useless things
-        group = VGroup(zero_deg_line, ray_angle, ray_angle_label)
-        self.play(Uncreate(group))
+        group = VGroup(zero_deg_line, ray_angle)
+        self.play(AnimationGroup(
+            Uncreate(group),
+            Unwrite(group_explain),
+            Unwrite(ray_angle_label)
+        ))
 
         self.wait()
 
@@ -156,17 +162,22 @@ class TrigonometryProofsScene(Scene):
 
         self.wait()
 
+        # move first triangle and write title text
         triangle_x_1_move = triangle_x_1.copy()
-        self.play(triangle_x_1_move.animate.move_to(RIGHT*3.5+UP))
-        self.play(triangle_x_1_move.animate.scale(5))
+        title_text = MathTex(r"\textrm{Horizontal intersections}").move_to(RIGHT*3.5+UP*3)
+        self.play(AnimationGroup(
+            triangle_x_1_move.animate.move_to(RIGHT*3.5+UP*1.25).scale(5),
+            Write(title_text)
+        ))
 
+        # write labels for each edge of the triangle and draw angle
         vertices = triangle_x_1_move.get_vertices()
-        hyp_label = MathTex("c")
+        hypotenuse_label = MathTex("c")
         center_coordinates = Line(vertices[0], vertices[1]).get_center()
-        hyp_label.move_to([center_coordinates[0]-0.3, center_coordinates[1]+0.3, center_coordinates[2]])
+        hypotenuse_label.move_to([center_coordinates[0]-0.3, center_coordinates[1]+0.3, center_coordinates[2]])
 
         edge_labels_and_angle = VGroup(
-            hyp_label,
+            hypotenuse_label,
             MathTex("1-py").next_to(Line(vertices[1], vertices[2])),
             MathTex("x_i").next_to(Line(vertices[2], vertices[0]), DOWN),
             Angle(
@@ -175,6 +186,34 @@ class TrigonometryProofsScene(Scene):
                 elbow=True
             )
         )
-        self.play(Write(edge_labels_and_angle))
+        self.play(Write(edge_labels_and_angle, run_time=1))
+
+        # write the proof
+        proof_x = MathTex(
+            r"""\sin{\theta} &= \frac{1 - py}{c} \Rightarrow c = \frac{1-py}{\sin{\theta}} \\
+                \cos{\theta} &= \frac{x_i}{c} \Rightarrow x_i = c \cdot \cos{\theta} \\
+                x_i &= \frac{(1-py)\cos{\theta}}{\sin{\theta}} = \frac{1-py}{\tan{\theta}}""",
+            font_size=32
+        ).move_to(RIGHT*3.5+DOWN*1.75)
+
+        self.play(Write(proof_x, run_time=5))
+        self.wait(5)
+
+        # simplify proof
+        xi_formula = MathTex(r"x_i = \frac{1-py}{\tan{\theta}}").move_to(RIGHT*3.5+DOWN*1.75)
+        self.play(Transform(proof_x, xi_formula))
+
+        self.wait()
+
+        # delete this triangle and formula
+        self.play(AnimationGroup(
+            Unwrite(proof_x),
+            Unwrite(edge_labels_and_angle),
+            Uncreate(triangle_x_1_move)
+        ))
+
+        # move second triangle
+        triangle_x_2_move = triangle_x_2.copy()
+        self.play(triangle_x_2_move.animate.move_to(RIGHT*3.5+UP*1.10).scale(3))
 
         self.wait()
